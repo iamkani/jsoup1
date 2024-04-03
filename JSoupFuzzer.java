3fde7ac0c0f4a28b477ecbe37e7cca9a79b4074d
iamkani <arunkaniyamattam@gmail.com>
2024-04-03 12:55:29 -0300

    Add files via upload

---
+++ JSoupFuzzer.java
@@ -0,0 +1,16 @@
+// Copyright 2021 Patrick Ventuzelo
+// Copyright 2021 Fuzzing Labs
+// 
+// Website: https://fuzzinglabs.com/
+
+import org.jsoup.Jsoup;
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+
+public class JSoupFuzzer {
+  public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+    //try {
+      Jsoup.parse(data.consumeRemainingAsString());
+    //} catch (IllegalArgumentException ignored) {
+    //}
+  }
+}
